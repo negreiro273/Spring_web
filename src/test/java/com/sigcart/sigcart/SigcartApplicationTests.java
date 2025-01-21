8a41606f78c70bfbf3d631d9eb6ea860f82f07e9
Uemerson Negreiro da Silva <Negreiro273@gmail.com>
2025-01-21 14:07:17 -0400

    Project Created

---
+++ src/test/java/com/sigcart/sigcart/SigcartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sigcart.sigcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigcartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
